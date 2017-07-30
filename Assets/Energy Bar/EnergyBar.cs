ed98172e0e77da90e99ca2a53adfa175f3daac80
isaac <isaaclpe@gmail.com>
2017-07-29 23:19:31 -0400

    Add first elements of the UI

---
+++ Assets/Energy Bar/EnergyBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class EnergyBar : MonoBehaviour {
+    public Transform container;
+    public GameObject element;
+    public int maxSegments = 10;
+    public bool debug;
+    [Range(0, 1)]
+    public float percent;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (debug) SetBarPercen(0);
+    }
+
+    public void SetBarPercen (float percen) {
+        int numSegments = Mathf.RoundToInt((debug ? percent : this.percent) * maxSegments);
+
+        var children = new List<GameObject>();
+        foreach (Transform child in container) children.Add(child.gameObject);
+        if (Application.isPlaying) {
+            children.ForEach(child => Destroy(child));
+        } else {
+            children.ForEach(child => DestroyImmediate(child));
+        }
+
+
+
+        for (int i = 0; i < numSegments; i++) {
+            GameObject.Instantiate(element, container, false);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/MovingStars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingStars : MonoBehaviour {
+    [Range(0, 1)]
+    public float stardensity;
+    public Color starColor;
+    public float speed;
+
+    #region references
+    public Image image;
+    public Texture2D texture;
+    #endregion
+
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
